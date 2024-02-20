bb38d277cc72e6d1190bf474c8fc83ae5ce8fbbf
bashar021 <alambashar021@gmail.com>
2024-02-21 00:30:24 +0530

    update in body

---
+++ src/components/Body.js
@@ -0,0 +1,170 @@
+import React from 'react'
+import '../test_styles/Body.css'
+import camera from '../test_images/camera.png'
+export default function Body() {
+    return (
+        <>
+            <div class="custom-container">
+                <div class="custom-header">
+                    <h2 class="custom-title">Best of electronics</h2>
+                    <a href="#" class="custom-arrow">→</a>
+                </div>
+                <div class="custom-scrollable-content">
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 1"></img>
+                            <p class="custom-image-name">Image 1</p>
+                            <p class="custom-price">$10</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                </div>
+            </div>
+            <div class="custom-container">
+                <div class="custom-header">
+                    <h2 class="custom-title">Best of toys</h2>
+                    <a href="#" class="custom-arrow">→</a>
+                </div>
+                <div class="custom-scrollable-content">
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 1"></img>
+                            <p class="custom-image-name">Image 1</p>
+                            <p class="custom-price">$10</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                </div>
+            </div>
+            <div class="custom-container">
+                <div class="custom-header">
+                    <h2 class="custom-title">Best of electronics</h2>
+                    <a href="#" class="custom-arrow">→</a>
+                </div>
+                <div class="custom-scrollable-content">
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 1"></img>
+                            <p class="custom-image-name">Image 1</p>
+                            <p class="custom-price">$10</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                    <div class="custom-item">
+                        <img src={camera} alt="Image 2"></img>
+                            <p class="custom-image-name">Image 2</p>
+                            <p class="custom-price">$15</p>
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,12 +3,14 @@ import Header from './Header.js'
 import Footer from './Footer.js'
 import Cata  from './Cata.js'
 import Slider from './Slider.js'
+import Body from './Body.js'
 export default function Homepage() {
   return (
     <>
     <Header></Header>
     <Cata></Cata>
     <Slider></Slider>
+    <Body></Body>
     <Footer></Footer>
     </>
   )
